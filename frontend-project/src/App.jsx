720082d5232a9fa0301d02397f88abf9f6e34a78
dikshaupadhyay0203 <dikshaupadhyay0203@gmail.com>
2026-02-27 13:57:00 +0530

    Added Login page and stored JWT token

--- frontend-project/src/App.jsx
+++ frontend-project/src/App.jsx
@@ -1,11 +1,13 @@
 import { BrowserRouter , Route, Routes, Navigate } from 'react-router-dom';
 import Register from './pages/Register';
+import Login from './pages/Login';
 function App(){
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Navigate to="/register" />} />
         <Route path="/register" element={<Register />} />
+        <Route path="/login" element={<Login />} />
       </Routes>
     </BrowserRouter>
   );
