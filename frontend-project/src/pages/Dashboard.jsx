e23b15c4f803c72c6b405f8cbbd6b32f77f0506d
dikshaupadhyay0203 <dikshaupadhyay0203@gmail.com>
2026-02-27 14:03:55 +0530

    Added Create Task functionality in Dashboard

--- frontend-project/src/pages/Dashboard.jsx
+++ frontend-project/src/pages/Dashboard.jsx
@@ -2,36 +2,88 @@ import { useEffect, useState } from "react";
 import axios from "axios";
 
 function Dashboard() {
-    const [tasks, setTasks] = useState([]);
 
-    const fetchTasks = async () => {
-        try {
-            const res= await axios.get("http://localhost:5000/api/tasks", {
-                headers: {
-                    Authorization: `Bearer ${localStorage.getItem("token")}`,
-                },
-            });
-            setTasks(res.data);
-        } catch (error) {
-            console.log("Error fetching tasks:", error);
+  const [tasks, setTasks] = useState([]);
+  const [title, setTitle] = useState("");
+  const [description, setDescription] = useState("");
+
+  const fetchTasks = async () => {
+    try {
+
+      const res = await axios.get("http://localhost:5000/api/tasks", {
+        headers: {
+          Authorization: "Bearer " + localStorage.getItem("token")
         }
-    };
+      });
 
-    useEffect(() => {
-        fetchTasks();
-    }, []);
-    return (
-        <div>
-            <h2>Dashboard</h2>
-            {tasks.map((task) => (
-                <div key={task._id}>
-                    <h3>{task.title}-{task.status}</h3>
-                    
-                </div>
-            ))}
+      setTasks(res.data);
+
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const createTask = async (e) => {
+    e.preventDefault();
+
+    try {
+
+      await axios.post("http://localhost:5000/api/tasks", 
+      { title, description },
+      {
+        headers: {
+          Authorization: "Bearer " + localStorage.getItem("token")
+        }
+      });
+
+      setTitle("");
+      setDescription("");
+
+      fetchTasks();
+
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(()=>{
+    fetchTasks();
+  }, []);
+
+  return (
+    <div>
+      <h2>Dashboard</h2>
+
+      <form onSubmit={createTask}>
+
+        <input
+          type="text"
+          placeholder="Task Title"
+          value={title}
+          onChange={(e)=>setTitle(e.target.value)}
+        />
+
+        <input
+          type="text"
+          placeholder="Description"
+          value={description}
+          onChange={(e)=>setDescription(e.target.value)}
+        />
+
+        <button type="submit">Create Task</button>
+
+      </form>
+
+      <h3>Your Tasks</h3>
+
+      {tasks.map((task)=>(
+        <div key={task._id}>
+          <p>{task.title} - {task.status}</p>
         </div>
-    );
+      ))}
+
+    </div>
+  );
 }
 
 export default Dashboard;
-
