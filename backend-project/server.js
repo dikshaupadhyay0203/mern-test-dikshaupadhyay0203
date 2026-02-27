faa6bd4011e4b94b90b20a35679682fc58419b1d
dikshaupadhyay0203 <dikshaupadhyay0203@gmail.com>
2026-02-27 10:26:42 +0530

    Added Register and Login API with JWT

--- backend-project/server.js
+++ backend-project/server.js
@@ -1,16 +1,15 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import connectDB from './config/db.js';
-
+import authRoutes from './routes/authRoutes.js';
 dotenv.config();
 
 connectDB();
 
 const app = express();
 app.use(express.json());
-
+app.use("/api/auth", authRoutes);
 app.listen(process.env.PORT || 6000, () => {
   console.log(`Server running on port ${process.env.PORT || 5000}`);
 });
 
-export default app;
