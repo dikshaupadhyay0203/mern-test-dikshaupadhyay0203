35677cf3c55f763e4c52e9496ffe35024f4e6259
dikshaupadhyay0203 <dikshaupadhyay0203@gmail.com>
2026-02-27 14:13:00 +0530

    Added Mark Completed button and useEffect

--- frontend-project/src/pages/Dashboard.jsx
+++ frontend-project/src/pages/Dashboard.jsx
@@ -45,10 +45,38 @@ function Dashboard() {
       console.log(error);
     }
   };
+  const updateTask = async (id) => {
+    try {
+      await axios.put(`http://localhost:5000/api/tasks/${id}`,
+        { status: "completed" },
+        {
+          headers: {
+            Authorization: "Bearer " + localStorage.getItem("token")
+          }
+        });
+      fetchTasks();
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
+  const deleteTask = async (id) => {
+    try {
+      await axios.delete(`http://localhost:5000/api/tasks/${id}`,
+        {
+            headers: {
+                Authorization: "Bearer " + localStorage.getItem("token")
+            }
+        });
+        fetchTasks();
+    } catch (error) {
+        console.log(error);
+    }
+  };
   useEffect(()=>{
     fetchTasks();
-  }, []);
+  },[]);
+ 
 
   return (
     <div>
@@ -79,6 +107,8 @@ function Dashboard() {
       {tasks.map((task)=>(
         <div key={task._id}>
           <p>{task.title} - {task.status}</p>
+            <button onClick={()=>updateTask(task._id)}>Update</button>
+          <button onClick={()=>deleteTask(task._id)}>Delete</button>
         </div>
       ))}
 
